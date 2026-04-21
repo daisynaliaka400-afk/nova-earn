d431489936b8f0b58751d7b9bfba6c6f4af989ca
daisy <daisynaliaka400@gmail.com>
2026-04-21 13:02:37 +0300

    Add public homepage landing page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Nova Earn!</h1>
+            <p>Your gateway to earning opportunities.</p>
+        </div>
+    );
+};
+
+export default Home;
